26fc10cee523400672bac1dd058d74e68a7070bb
jloomisVCE <jloomis@vtecostudies.org>
2023-09-14 14:36:25 -0400

    ingesting data

--- sql/create_tables_loon_watch_ingest.sql
+++ sql/create_tables_loon_watch_ingest.sql
@@ -1,15 +1,17 @@
 
-drop table if exists loonWatch_ingest;
+--create table loonwatch_ingest_backup as select * from loonWatch_ingest;
+
+--drop table if exists loonWatch_ingest;
 
 create table if not exists loonWatch_ingest (
 	--lwIngestId SERIAL,
 	lwIngestLocation TEXT NOT NULL,
 	lwIngestTownName TEXT NOT NULL,
 	--lwIngestTownId INTEGER,
-	lwIngestDate DATE NOT NULL,
-	--lwIngestObserverEmail TEXT[], --NOT NULL, --Initial import has no observer info
-	--lwIngestObserverName TEXT[], --NOT NULL, --Initial import has no observer info
+	lwIngestObserverName TEXT, --Initial import has a few delimited values
+	--lwIngestObserverEmail TEXT, --Initial import has no observer info
 	--lwIngestObserverCount INTEGER,
+	lwIngestDate DATE NOT NULL,
 	lwIngestStart TIME WITHOUT TIME ZONE,
 	lwIngestStop TIME WITHOUT TIME ZONE,
 	lwIngestAdult INTEGER DEFAULT 0,
@@ -29,7 +31,7 @@ ALTER TABLE loonWatch_ingest ADD CONSTRAINT fk_location
 --	FOREIGN KEY (lwIngestTownId) REFERENCES vt_town ("townId");
 ALTER TABLE loonWatch_ingest DROP CONSTRAINT fk_location;
 --Import data using psql:
---\COPY loonWatch_ingest FROM 'C:\Users\jtloo\Documents\VCE\LoonWeb\loonWatchData\csv_import\LoonWatch_Raw_Counts_2011.csv' DELIMITER ',' CSV HEADER
+--\COPY loonWatch_ingest FROM 'C:\Users\jtloo\Documents\VCE\LoonWeb\dbLoonWatch\csv_import\LoonWatch_Raw_Counts_2011.csv' DELIMITER ',' CSV HEADER
 
 --5 location names from 2011 spreadsheet have leading/trailing whitespace, so they don't match our corrected loon_locations
 update loonWatch_ingest set lwIngestLocation=TRIM(BOTH FROM lwIngestLocation);
@@ -53,3 +55,7 @@ left join vt_water_body on wbTextId=waterBodyId
 where waterBodyId is null; --3
 
 update loonWatch_ingest set lwIngestLocation='Wrightsville' where lwIngestLocation = 'WrightSurveyille';
+
+--delete from loonWatch_ingest where date_part('year', lwingestdate)=2014; --to fix an incorrect ingestion
+select * from loonWatch_ingest where date_part('year', lwingestdate)=2015;
+
